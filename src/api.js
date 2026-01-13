69008c57aa412c465763941f183c808386d3149c
KRITI <119161632+Kriti4242@users.noreply.github.com>
2026-01-13 12:44:06 +0530

    Update API base URL to production server

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+ baseURL: "https://myspace-fsp3.onrender.com"
 })
 
 API.interceptors.request.use(req => {
@@ -10,3 +10,4 @@ API.interceptors.request.use(req => {
 })
 
 export default API
+
