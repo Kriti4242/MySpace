ff21651519c56afec8ca614c9d01de5714457097
KRITI <119161632+Kriti4242@users.noreply.github.com>
2026-01-13 15:55:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Dashboard from "./pages/Dashboard"
-import Forgot from "./pages/Forgot"
+import Forgot from "./pages/ForgotPassword"
 import ResetPassword from "./pages/ResetPassword"
 
 const PrivateRoute = ({ children }) => {
