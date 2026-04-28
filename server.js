d2355bc19bd203b90a980d834f9a7de8b117d0eb
KRITI <kritiverma2424@gmail.com>
2026-04-28 11:11:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,11 @@ app.use(express.json())
 
 // ✅ MongoDB
 mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err))
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => {
+    console.log("DB ERROR:", err);
+    process.exit(1); 
+  });
 
 // ✅ Routes
 app.use("/auth", require("./routes/auth"))
@@ -28,8 +31,10 @@ app.use("/jobs", require("./routes/jobs"))
 app.use("/notes", require("./routes/notes"))
 
 // ✅ Server
-app.listen(5000, () => {
-  console.log("Server running on port 5000")
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
 
 
